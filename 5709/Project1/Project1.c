2166d73bbcca967ce9ebe78f6e6eaf41c6c1b053
qfgg700 <qfgg700@gmail.com>
2020-08-21 21:35:39 +0800

    luogu5709

---
+++ 5709/Project1/Project1.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+int main()
+{
+    double m, t, s, a, rest;
+    int b;
+    scanf_s("%lf %lf %lf", &m, &t, &s);
+    if (t == 0) printf("0");
+    else
+    {
+        a = s / t;
+        b = (int)a;
+        rest = m - (double)b;
+        if ((int)a != a) rest--;
+        if (rest < 0) printf("0");
+        else printf("%.0lf", rest);
+    }
+    return 0;
+}
